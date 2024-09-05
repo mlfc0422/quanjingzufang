865b544aa0554f4dd03bfe8e6ee8b2a627d9cba8
mlfc <mlfc0422@outlook.com>
2024-09-05 09:01:17 +0800

    加入测试

--- fangyuan/src/main/java/com/mlfc/fangyuan/consumer/controller/FangyuanController.java
+++ fangyuan/src/main/java/com/mlfc/fangyuan/consumer/controller/FangyuanController.java
@@ -2,6 +2,7 @@ package com.mlfc.fangyuan.consumer.controller;
 
 import apiserrvice.FangyuanService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,14 @@ public class FangyuanController {
     @DubboReference
     private FangyuanService fangyuanService;
 
+    @GetMapping("/getLocalDateTime")
     public String getLocalDateTime() {
         return fangyuanService.getLocalDateTime().toString();
+    }
+
+    @GetMapping("/test")
+    public void test() {
+        System.out.println("test");
+
     }
 }
