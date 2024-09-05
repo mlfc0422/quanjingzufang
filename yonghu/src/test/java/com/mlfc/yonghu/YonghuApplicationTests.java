7c3726c96b0c63bc7d2fc7504f5d1c2933e11e9b
mlfc <mlfc0422@outlook.com>
2024-09-05 19:36:50 +0800

    增加用户服务

---
+++ yonghu/src/test/java/com/mlfc/yonghu/YonghuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlfc.yonghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonghuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
