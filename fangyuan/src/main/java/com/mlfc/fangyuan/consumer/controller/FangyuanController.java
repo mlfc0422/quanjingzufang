25d1ea3f9c76554c0f916d5da26665588129704b
ray <1392505897@qq.com>
2024-09-12 11:05:52 +0800

    更新房源查询，删除，新增功能

--- fangyuan/src/main/java/com/mlfc/fangyuan/consumer/controller/FangyuanController.java
+++ fangyuan/src/main/java/com/mlfc/fangyuan/consumer/controller/FangyuanController.java
@@ -1,16 +1,64 @@
 package com.mlfc.fangyuan.consumer.controller;
 
 import apiserrvice.FangyuanService;
+import common.Rest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pojo.HouseResources;
 
+import java.util.Collections;
+import java.util.List;
+
+@RequestMapping("fangyuan")
 @RestController
+@Slf4j
 public class FangyuanController {
 
     @DubboReference
     private FangyuanService fangyuanService;
 
 
+    @GetMapping("/{id}")
+    public Rest<HouseResources> QueryFYById(@RequestBody Long id) {
+        HouseResources houseResources = fangyuanService.getById(id);
+        // 检查结果是否为空，如果为空则返回404
+        if (houseResources == null) {
+            return Rest.error("无无无");
+        }
+
+        // 如果找到了资源，返回200状态码和资源
+        return Rest.success(houseResources);
+    }
+
+    @GetMapping("/all")
+    public List<HouseResources> QueryFY() {
+        return fangyuanService.list();
+    }
+
+    @PostMapping("/delete/{id}")
+    public Rest<String> deleteByIds(@RequestBody Long params){
+
+        fangyuanService.removeByIds(Collections.singleton(params.longValue()));
+
+        // 如果找到了资源，返回200状态码和资源
+        return Rest.success("删除成功");
+    }
 
 
+    @PostMapping("/add")
+    public Rest<String> addHouseResource(@RequestBody HouseResources houseResources) {
+        try {
+            boolean saved = fangyuanService.save(houseResources);
+            if (saved) {
+                return Rest.success("房源信息添加成功。");
+            } else {
+                return Rest.error("房源信息添加失败。");
+            }
+        } catch (Exception e) {
+            // 记录异常日志
+            return Rest.error("服务器错误，房源信息添加失败。");
+        }
+    }
 }
+
--- fangyuan/src/main/java/com/mlfc/fangyuan/provider/impl/FangyuanServiceImpl.java
+++ fangyuan/src/main/java/com/mlfc/fangyuan/provider/impl/FangyuanServiceImpl.java
@@ -1,12 +1,20 @@
 package com.mlfc.fangyuan.provider.impl;
 
 import apiserrvice.FangyuanService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mlfc.fangyuan.provider.mapper.FangyuanMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pojo.HouseResources;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 @DubboService
 @Slf4j
-public class FangyuanServiceImpl implements FangyuanService {
+public class FangyuanServiceImpl extends ServiceImpl<FangyuanMapper , HouseResources>implements FangyuanService {
 
 }
